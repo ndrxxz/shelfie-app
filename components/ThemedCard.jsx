dfdbcd1542b49664e24c5a8eefb9cb0ad4754924
Andrea <80144630+ndrxxz@users.noreply.github.com>
2026-07-03 15:28:51 +0800

    create themed card

---
+++ components/ThemedCard.jsx
@@ -0,0 +1,24 @@
+import { StyleSheet, useColorScheme, View } from "react-native";
+import React from "react";
+import { Colors } from "@/constants";
+
+const ThemedCard = ({ style, ...props }) => {
+  const colorScheme = useColorScheme();
+  const theme = Colors[colorScheme] ?? Colors.light;
+
+  return (
+    <View 
+      style={[{ backgroundColor: theme.uiBackground }, styles.card, style]}
+      {...props}
+    />
+  );
+};
+
+export default ThemedCard;
+
+const styles = StyleSheet.create({
+  card: {
+    borderRadius: 5,
+    padding: 20
+  }
+});
