6a48cd1cd3a4b7997e0d63762714405bdc0fe2fe
Andrea <80144630+ndrxxz@users.noreply.github.com>
2026-07-03 18:01:28 +0800

    update barrel file

--- components/index.js
+++ components/index.js
@@ -3,3 +3,4 @@ export { default as ThemedCard } from "./ThemedCard";
 export { default as ThemedLogo } from "./ThemedLogo";
 export { default as Spacer } from "./Spacer";
 export { default as ThemedText } from "./ThemedText";
+export { default as ThemedButton } from "./ThemedButton";
