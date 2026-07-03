e2d890e63a1cc23453da86678d886a25b3d28779
Andrea <80144630+ndrxxz@users.noreply.github.com>
2026-07-03 15:28:25 +0800

    add barrel file

---
+++ components/index.js
@@ -0,0 +1,5 @@
+export { default as ThemedView } from "./ThemedView";
+export { default as ThemedCard } from "./ThemedCard";
+export { default as ThemedLogo } from "./ThemedLogo";
+export { default as Spacer } from "./Spacer";
+export { default as ThemedText } from "./ThemedText";
